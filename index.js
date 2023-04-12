cbf3c9a740998d3cd8f67f1d8d286b3edf59920f
LodgeSec <secretary@lodge804.com>
2023-04-12 10:35:46 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+// Serve static files from the "public" directory
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})

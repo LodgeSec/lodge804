ee796c8c308ad6c7f1f122163462b15f45250124
secretary <secretary@lodge804.com>
2024-02-07 21:33:30 -0500

    Update Site for Spring 2024 changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Serve static files from the "public" directory
-app.use(express.static('public', {index: 'index.html'}))
+app.use(express.static('public', {index: 'Home.html'}))
 
 const port = process.env.PORT || 8080
 app.listen(port, () => {
